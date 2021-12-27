199a4e3a37986a2d435950debf0d236ec1646dd4
Keemperor <jacobotau@hotmail.com>
2021-12-26 23:10:42 -0500

    initiated questions

--- Develop/index.js
+++ Develop/index.js
@@ -3,10 +3,31 @@ const fs = require('fs');
 const markdowns = require('./utils/generateMarkdown.js');
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the projects title? (Required)',
+    validate: projectName =>  {
+      if (projectName) {
+        return true;
+      }
+      else {
+        console.log('A project name is required to continue!');
+        return false;
+      }
+    }
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err)
+    throw err;
+    console.log('Accomplished! Information succesfully submitted to the README file')
+  });
+};
 
 // TODO: Create a function to initialize app
 function init() {
